5b84d242612f14492357e3faef7973dba044fcb2
Hung Truong <truongxh1@posteo.de>
2021-02-03 23:47:50 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/app-release-unsigned.apk -m "v${GITHUB_REF##*/}" ${GITHUB_REF##*/} 
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/*-release.apk -m "v${GITHUB_REF##*/}" "Release v${GITHUB_REF##*/}" 
